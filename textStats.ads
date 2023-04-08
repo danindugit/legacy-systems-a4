daa7f961ed2881a390cbf42464b19224056671fe
Danindu Marasinghe <dmarasin@uoguelph.ca>
2023-04-07 23:00:18 -0400

    renamed + skeleton

--- textStats.ads
+++ textStats.ads
@@ -1,3 +1,4 @@
 --  Name: Danindu Marasinghe
 --  Student ID: 1093791
 --  Course: CIS*3190
+
--- textyzer.adb
+++ textyzer.adb
@@ -1,3 +1,9 @@
 --  Name: Danindu Marasinghe
 --  Student ID: 1093791
 --  Course: CIS*3190
+
+procedure textyzer is
+   
+begin
+   
+end textyzer;
